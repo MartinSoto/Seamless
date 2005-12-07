473dc8e598b7381c585def115d0d76dc79e52f54
Martin Soto <soto@freedesktop.org>
2005-12-07 00:04:41 +0000

    Update event creation to 0.10
    2005-12-07  Martin Soto  <martinsoto@users.sourceforge.net>
    
    	* src/player/events.py (createCustom, audio, eos, filler, flush) 
    	(newsegment, highlight, nav, stillFrame, subpictureClut) 
    	(subpicture, subpictureHide, subpictureShow): Update to GStreamer
    	0.10.
    
    
    git-archimport-id: soto@freedesktop.org--2004-personal/seamless--port09--0.1--patch-7

--- src/player/events.py
+++ src/player/events.py
@@ -23,32 +23,42 @@ def mpegTimeToGstTime(mpegTime):
     """Convert MPEG time values to the GStreamer time format."""
     return (long(mpegTime) * gst.MSECOND) / 90
 
+def createCustom(st):
+    """Create a custom downstream event containing `st`."""
+    return gst.event_new_custom(gst.EVENT_CUSTOM_DOWNSTREAM, st)
+
 def audio(physical):
     """Create and return a new audio event for the specified physical
     stream."""
-    st = gst.Structure('application/x-gst-dvd');
+    st = gst.Structure('application/x-gst-dvd')
     st.set_value('event', 'dvd-audio-stream-change')
     st.set_value('physical', physical, 'int')
-    return gst.event_new_any(st)
+    return createCustom(st)
 
 def eos():
     """Create and return an end of stream (EOS) event."""
-    return gst.Event(gst.EVENT_EOS)
+    return gst.event_new_eos()
 
 def filler():
     """Create and return a filler event."""
-    return gst.Event(gst.EVENT_FILLER)
+    return gst.event_new_filler()
 
 def flush():
     """Create and return a flush event."""
-    return gst.Event(gst.EVENT_FLUSH)
+    return gst.event_new_flush_start()
+
+def newsegment(update, startTime, endTime):
+    """Create and return a newsegment event for the specified start
+    and end times. Times are specified in nanoseconds."""
+    return gst.event_new_new_segment(update, 1.0, gst.FORMAT_TIME,
+                                     startTime, endTime, 0)
 
 def highlight(area, button, palette):
     """Create and return a new highlight event based on the specified
     highlight area, button number, and color palette."""
     (sx, sy, ex, ey) = area
 
-    st = gst.Structure('application/x-gst-dvd');
+    st = gst.Structure('application/x-gst-dvd')
     st.set_value('event', 'dvd-spu-highlight')
     st.set_value('button', button)
     st.set_value('palette', palette)
@@ -57,61 +67,52 @@ def highlight(area, button, palette):
     st.set_value('ex', ex)
     st.set_value('ey', ey)
 
-    return gst.event_new_any(st)
+    return createCustom(st)
 
 def highlightReset():
     """Create and return a new highlight reset event."""
     st = gst.Structure('application/x-gst-dvd')
     st.set_value('event', 'dvd-spu-reset-highlight')
 
-    return gst.event_new_any(st)
-
-def nav(startTime, endTime):
-    """Create and return a nav packet event for the specified start
-    and end times."""
-    st = gst.Structure('application/x-gst-dvd')
-    st.set_value('event', 'dvd-nav-packet')
-    st.set_value('start_ptm', mpegTimeToGstTime(startTime), 'uint64')
-    st.set_value('end_ptm', mpegTimeToGstTime(endTime), 'uint64')
-    return gst.event_new_any(st)
+    return createCustom(st)
 
 def stillFrame():
     """Create and return a still frame event."""
-    st = gst.Structure('application/x-gst-dvd');
+    st = gst.Structure('application/x-gst-dvd')
     st.set_value('event', 'dvd-spu-still-frame')
-    return gst.event_new_any(st)
+    return createCustom(st)
 
 def subpictureClut(clut):
     """Create and return a new subpicture CLUT event based on the
     specified color lookup table (an 16 position array)."""
-    st = gst.Structure('application/x-gst-dvd');
+    st = gst.Structure('application/x-gst-dvd')
     st.set_value('event', 'dvd-spu-clut-change')
 
     # Each value is stored in a separate field.
     for i in range(16):
         st.set_value('clut%02d' % i, clut[i])
 
-    return gst.event_new_any(st)
+    return createCustom(st)
 
 def subpicture(physical):
     """Create and return a new subpicture event for the specified
     physical stream."""
-    st = gst.Structure('application/x-gst-dvd');
+    st = gst.Structure('application/x-gst-dvd')
     st.set_value('event', 'dvd-spu-stream-change')
     st.set_value('physical', physical, 'int')
-    return gst.event_new_any(st)
+    return createCustom(st)
 
 def subpictureHide():
     """Create and return a new subpicture hide event."""
     st = gst.Structure('application/x-gst-dvd')
     st.set_value('event', 'dvd-spu-hide')
 
-    return gst.event_new_any(st)
+    return createCustom(st)
 
 def subpictureShow():
     """Create and return a new subpicture show event."""
     st = gst.Structure('application/x-gst-dvd')
     st.set_value('event', 'dvd-spu-show')
 
-    return gst.event_new_any(st)
+    return createCustom(st)
 
